c1fba962912acbd3b035303c7b9d530bc34060ff
Robert <robertariascastro@outlook.com>
2023-04-08 16:28:59 -0600

    feat: create and enable the main graphql schema

---
+++ web/modules/custom/rac_graphql/src/Plugin/GraphQL/Schema/MainSchema.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\rac_graphql\Plugin\GraphQL\Schema;
+
+use Drupal\graphql\GraphQL\ResolverBuilder;
+use Drupal\graphql\GraphQL\ResolverRegistry;
+use Drupal\graphql\GraphQL\ResolverRegistryInterface;
+use Drupal\graphql\Plugin\GraphQL\Schema\SdlSchemaPluginBase;
+
+/**
+ * @Schema(
+ *   id = "rac_main",
+ *   name = @Translation("Main Schema"),
+ *   description = @Translation("The main schema entrypoint")
+ * )
+ */
+class MainSchema extends SdlSchemaPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getResolverRegistry(): ResolverRegistryInterface {
+    $builder = new ResolverBuilder();
+    $registry = new ResolverRegistry();
+
+    $this->addQueryFields($registry, $builder);
+    $this->addNodeLandingPageFields($registry, $builder);
+
+    return $registry;
+  }
+
+  /**
+   * Add Query field resolvers.
+   *
+   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
+   *   The resolver registry.
+   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
+   *   The resolver builder.
+   */
+  protected function addQueryFields(ResolverRegistry $registry, ResolverBuilder $builder): void {
+    $registry->addFieldResolver('Query', 'landingPage',
+      $builder->produce('entity_load')
+        ->map('type', $builder->fromValue('node'))
+        ->map('bundles', $builder->fromValue(['landing_page']))
+        ->map('id', $builder->fromArgument('id'))
+    );
+  }
+
+  /**
+   * Add Query field resolvers.
+   *
+   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
+   *   The resolver registry.
+   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
+   *   The resolver builder.
+   */
+  protected function addNodeLandingPageFields(ResolverRegistry $registry, ResolverBuilder $builder): void {
+    $registry->addFieldResolver('NodeLandingPage', 'id',
+      $builder->produce('entity_id')
+        ->map('entity', $builder->fromParent())
+    );
+
+    $registry->addFieldResolver('NodeLandingPage', 'title',
+      $builder->compose(
+        $builder->produce('entity_label')
+          ->map('entity', $builder->fromParent()),
+        $builder->produce('uppercase')
+          ->map('string', $builder->fromParent())
+      )
+    );
+
+    $registry->addFieldResolver('NodeLandingPage', 'author',
+      $builder->compose(
+        $builder->produce('entity_owner')
+          ->map('entity', $builder->fromParent()),
+        $builder->produce('entity_label')
+          ->map('entity', $builder->fromParent())
+      )
+    );
+  }
+
+}
