a1a2d09bfd5c4636675daf222776ac1d06a57a17
Robert <robertariascastro@outlook.com>
2023-04-15 14:35:12 -0600

    feat: create resolver to load by revision id

---
+++ web/modules/custom/rac_graphql/src/Plugin/GraphQL/DataProducer/Entity/EntityLoadByRevisionId.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace Drupal\rac_graphql\Plugin\GraphQL\DataProducer\Entity;
+
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\TranslatableInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\graphql\GraphQL\Buffers\EntityRevisionBuffer;
+use Drupal\graphql\GraphQL\Execution\FieldContext;
+use Drupal\graphql\Plugin\GraphQL\DataProducer\DataProducerPluginBase;
+use GraphQL\Deferred;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Loads an entity by revision ID.
+ *
+ * @DataProducer(
+ *   id = "entity_load_by_revision_id",
+ *   name = @Translation("Load entity by revision id"),
+ *   description = @Translation("Loads a single entity by revision id."),
+ *   produces = @ContextDefinition("entity",
+ *     label = @Translation("Entity")
+ *   ),
+ *   consumes = {
+ *     "type" = @ContextDefinition("string",
+ *       label = @Translation("Entity type")
+ *     ),
+ *     "revision_id" = @ContextDefinition("string",
+ *       label = @Translation("Revision ID")
+ *     ),
+ *     "language" = @ContextDefinition("string",
+ *       label = @Translation("Entity language"),
+ *       required = FALSE
+ *     ),
+ *     "bundles" = @ContextDefinition("string",
+ *       label = @Translation("Entity bundle(s)"),
+ *       multiple = TRUE,
+ *       required = FALSE
+ *     ),
+ *     "access" = @ContextDefinition("boolean",
+ *       label = @Translation("Check access"),
+ *       required = FALSE,
+ *       default_value = TRUE
+ *     ),
+ *     "access_user" = @ContextDefinition("entity:user",
+ *       label = @Translation("User"),
+ *       required = FALSE,
+ *       default_value = NULL
+ *     ),
+ *     "access_operation" = @ContextDefinition("string",
+ *       label = @Translation("Operation"),
+ *       required = FALSE,
+ *       default_value = "view"
+ *     )
+ *   }
+ * )
+ */
+class EntityLoadByRevisionId extends DataProducerPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * The entity repository service.
+   *
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
+   */
+  protected EntityRepositoryInterface $entityRepository;
+
+  /**
+   * The entity buffer service.
+   *
+   * @var \Drupal\graphql\GraphQL\Buffers\EntityRevisionBuffer
+   */
+  protected EntityRevisionBuffer $entityBuffer;
+
+  /**
+   * EntityLoadByRevisionId constructor.
+   *
+   * @param array $configuration
+   *   The plugin configuration array.
+   * @param string $pluginId
+   *   The plugin id.
+   * @param array $pluginDefinition
+   *   The plugin definition array.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager service.
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entityRepository
+   *   The entity repository service.
+   * @param \Drupal\graphql\GraphQL\Buffers\EntityRevisionBuffer $entityBuffer
+   *   The entity buffer service.
+   */
+  public function __construct(
+    array $configuration,
+    $pluginId,
+    array $pluginDefinition,
+    EntityTypeManagerInterface $entityTypeManager,
+    EntityRepositoryInterface $entityRepository,
+    EntityRevisionBuffer $entityBuffer
+  ) {
+    parent::__construct($configuration, $pluginId, $pluginDefinition);
+    $this->entityTypeManager = $entityTypeManager;
+    $this->entityRepository = $entityRepository;
+    $this->entityBuffer = $entityBuffer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('entity.repository'),
+      $container->get('graphql.buffer.entity_revision')
+    );
+  }
+
+  /**
+   * Resolves the entity by its revision ID.
+   *
+   * @param string $type
+   *   The entity type.
+   * @param int $revision_id
+   *   The revision ID of the entity to be resolved.
+   * @param string|null $language
+   *   The language translation to get the content.
+   * @param array|null $bundles
+   *   The allowed bundles to load.
+   * @param bool|null $access
+   *   Whether to check access to the entity by user.
+   * @param \Drupal\Core\Session\AccountInterface|null $accessUser
+   *   The user fetching the data.
+   * @param string|null $accessOperation
+   *   The operation being performed.
+   * @param \Drupal\graphql\GraphQL\Execution\FieldContext $context
+   *   The field context.
+   *
+   * @return \GraphQL\Deferred
+   *   The loaded entity.
+   */
+  public function resolve(string $type, int $revision_id, ?string $language, ?array $bundles, ?bool $access, ?AccountInterface $accessUser, ?string $accessOperation, FieldContext $context) {
+    $resolver = $this->entityBuffer->add($type, $revision_id);
+
+    return new Deferred(function () use ($type, $language, $bundles, $resolver, $context, $access, $accessUser, $accessOperation) {
+      if (!$entity = $resolver()) {
+        // If there is no entity with this id, add the list cache tags so that
+        // the cache entry is purged whenever a new entity of this type is
+        // saved.
+        $type = $this->entityTypeManager->getDefinition($type);
+        /** @var \Drupal\Core\Entity\EntityTypeInterface $type */
+        $tags = $type->getListCacheTags();
+        $context->addCacheTags($tags);
+        return NULL;
+      }
+
+      $context->addCacheableDependency($entity);
+      if (isset($bundles) && !in_array($entity->bundle(), $bundles)) {
+        // If the entity is not among the allowed bundles, don't return it.
+        return NULL;
+      }
+
+      // Get the correct translation.
+      if (isset($language) && $language != $entity->language()->getId() && $entity instanceof TranslatableInterface) {
+        $entity = $entity->getTranslation($language);
+        $entity->addCacheContexts(["static:language:{$language}"]);
+      }
+
+      // Check if the passed user (or current user if none is passed) has access
+      // to the entity, if not return NULL.
+      if ($access) {
+        /** @var \Drupal\Core\Access\AccessResultInterface $accessResult */
+        $accessResult = $entity->access($accessOperation, $accessUser, TRUE);
+        $context->addCacheableDependency($accessResult);
+        if (!$accessResult->isAllowed()) {
+          return NULL;
+        }
+      }
+
+      return $entity;
+    });
+  }
+
+}
